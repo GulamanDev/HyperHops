93b603bc9e15aa80bd696f8cb8eb89069380ec5e
mykrophones <laomyk0522@gmail.com>
2024-11-29 23:05:19 +0800

    Offline Mode for testing

---
+++ HyperHops/Assets/Scripts/OfflineMode.cs
@@ -0,0 +1,21 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class OfflineModeManager : MonoBehaviour
+{
+    private void Start()
+    {
+        PhotonNetwork.OfflineMode = true;
+
+        PhotonNetwork.JoinOrCreateRoom("OfflineRoom", new Photon.Realtime.RoomOptions(), null);
+
+        SpawnPlayer();
+    }
+
+    void SpawnPlayer()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-5f, 5f), 0, Random.Range(-5f, 5f));
+
+        PhotonNetwork.Instantiate("PlayerPrefab", spawnPosition, Quaternion.identity);
+    }
+}
